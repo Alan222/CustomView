fb882710905dd7654b7e78be0e5c7f7363052cc6
lin <QQlin147>
2017-05-11 15:26:10 +0800

    chartView添加数据

--- app/src/main/java/charview/com/mychartview/ChartActivity.java
+++ app/src/main/java/charview/com/mychartview/ChartActivity.java
@@ -1,13 +1,32 @@
 package charview.com.mychartview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import charview.com.custom.MyChartView;
 
 public class ChartActivity extends AppCompatActivity {
+
+    @BindView(R.id.chart)
+    MyChartView mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
+        ButterKnife.bind(this);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(30);
+        list.add(40);
+        list.add(60);
+        list.add(45);
+        list.add(46);
+        list.add(65);
+        list.add(36);
+        mChart.setItemData(list);
     }
 }
--- app/src/main/java/charview/com/mychartview/MainActivity.java
+++ app/src/main/java/charview/com/mychartview/MainActivity.java
@@ -1,7 +1,12 @@
 package charview.com.mychartview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.chartView, R.id.waveView})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.chartView:
+                startActivity(new Intent(this,ChartActivity.class));
+                break;
+            case R.id.waveView:
+                startActivity(new Intent(this,WaveActivity.class));
+                break;
+        }
     }
 }
